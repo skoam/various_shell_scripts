b9e98ff300c3b7e243925a93670400a54cef37cc
Sylvius Fischer <sylvius@fumiko-game.com>
2019-03-08 11:09:47 +0100

    Fixed a typo

--- add_string_at_position.sh
+++ add_string_at_position.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# USAGE: ./add_string_at_position -i "Test Input String" -p 12 -f /path/to/file.txt
+# USAGE: ./add_string_at_position.sh -i "Test Input String" -p 12 -f /path/to/file.txt
 
 while [ "$1" != "" ]; do
   case $1 in
